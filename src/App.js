282ed837d2eadd06d238b18282191897ebdadecf
AnthonyPillow <anthonypillow18@gmail.com>
2020-05-21 17:29:37 -0700

    Did some restructuring and used some code from previous projects that works. Started work on the nav component.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Nav from "./components/Nav/index"
+import Home from "./pages/Home"
+import About from "./pages/About"
+import Contact from "./pages/Contact"
+import Projects from "./pages/Projects"
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  return(
+    <div>
+      <Nav />
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route exact path="/about">
+            <About />
+          </Route>
+          <Route exact path="/projects">
+            <Projects />
+          </Route>
+          <Route exact path="/contact">
+            <Contact />
+          </Route>
+        </Switch>
+      </Router>
+
     </div>
-  );
+
+  ) 
 }
 
 export default App;
---
+++ src/components/Nav/index.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { Row, Col, Container } from "../Grid/index";
+import "./style.css"
+
+function Nav() {
+  return (
+    <Row>
+      <Col size="md-3">
+        <div className="squid">adfadf</div>
+      </Col>
+      <Col size="md-3">
+        <p>Hello</p>
+      </Col>
+      <Col size="md-3">
+        <p>Hello</p>
+      </Col>
+      <Col size="md-3">
+        <p>Hello</p>
+      </Col>
+    </Row>
+  );
+}
+
+export default Nav;
