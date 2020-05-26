5a9cb145b19490bcf5911336ad88a5bd3108007f
AnthonyPillow <anthonypillow18@gmail.com>
2020-05-26 12:35:45 -0700

    Added wordbox component

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ import "./App.css";
 
 function App() {
   return (
-    <div>
+    <div className="wrapper container-fluid">
       <Router>
         <Nav />
         <Switch>
--- src/pages/About.js
+++ src/pages/About.js
@@ -1,7 +1,21 @@
-import React from "react"
+import React from "react";
+import WordBox from "../components/WordBox/index";
+import "./style.css";
 
 function About() {
-    return <h1>shit</h1>
+  return (
+    <>
+      <WordBox
+        words="Hey there! I'm Anthony Pillow, an aspiring full stack web developer. I
+   have a wide range of knowledge of languages used in web development
+   with a heavier focus on the MERN stack."
+      />
+
+      <WordBox words="I'm always looking for ways to improve myself, my code, and I'm always open to new ideas as well as suggestions." />
+
+      
+    </>
+  );
 }
 
-export default About
+export default About;
