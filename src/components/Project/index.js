ad6768fe6b57ef6446a3837d732670f6f8f94c0f
AnthonyPillow <anthonypillow18@gmail.com>
2020-06-02 11:04:16 -0700

    I've been trying to fix the image problem on the cards on my project component for awhile now.

--- src/components/Project/index.js
+++ src/components/Project/index.js
@@ -1,14 +1,17 @@
 import React from "react";
 import "./style.css";
+import Images from "../Images/index";
 
 function Project(props) {
   return (
     <div className="project-container">
-      <img
-        className="project-images"
-        src={require("./Squid.jpg")}
-        alt="Squid"
-      ></img>
+      {Images.map((el) => {
+        if (el.src !== props.imgpath) {
+        } else
+          return (
+            <img className="project-images" src={el.src} alt="Squid"></img>
+          );
+      })}
       <div className="links-container">
         <a href={props.deployed}>{props.title}</a>
         <br />
