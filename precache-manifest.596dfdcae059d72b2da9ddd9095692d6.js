4b8b3db5f191379863ebfaf3c346277932679acf
AnthonyPillow <anthonypillow18@gmail.com>
2020-06-29 15:51:34 -0700

    Updates

---
+++ precache-manifest.596dfdcae059d72b2da9ddd9095692d6.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "89ceb7456bfe3d5168bb186a5ed36f77",
+    "url": "/react-portfolio/about/index.html"
+  },
+  {
+    "revision": "df37705acb29691dd53a",
+    "url": "/react-portfolio/about/static/css/main.41316d4b.chunk.css"
+  },
+  {
+    "revision": "e4295da9855dce401de0",
+    "url": "/react-portfolio/about/static/js/2.41ee9d9b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-portfolio/about/static/js/2.41ee9d9b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "df37705acb29691dd53a",
+    "url": "/react-portfolio/about/static/js/main.543f4b0f.chunk.js"
+  },
+  {
+    "revision": "a635f5a2d14a61fec0ea",
+    "url": "/react-portfolio/about/static/js/runtime-main.f24412a1.js"
+  },
+  {
+    "revision": "77829a83deb81cc323d04119da705ad8",
+    "url": "/react-portfolio/about/static/media/Squid.77829a83.jpg"
+  },
+  {
+    "revision": "047daae16b9e916061661ddf0e30d3a5",
+    "url": "/react-portfolio/about/static/media/art.047daae1.jpg"
+  },
+  {
+    "revision": "488ec6dbbb7f265b7d242adb4d2e445d",
+    "url": "/react-portfolio/about/static/media/bg.488ec6db.jpg"
+  },
+  {
+    "revision": "6fe07faf09ad295ba572370a83857f7f",
+    "url": "/react-portfolio/about/static/media/daily-planner.6fe07faf.jpg"
+  },
+  {
+    "revision": "fcbce422e044f87cb220b8c0deeaa368",
+    "url": "/react-portfolio/about/static/media/finance.fcbce422.jpg"
+  },
+  {
+    "revision": "302f79751fd6fffb69dc83ba2b529956",
+    "url": "/react-portfolio/about/static/media/hippo.302f7975.jpg"
+  },
+  {
+    "revision": "914e6532e297c526e04a3d0aa729d7aa",
+    "url": "/react-portfolio/about/static/media/me.914e6532.jpg"
+  }
+]);
